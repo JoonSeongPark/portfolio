31032cc1bca4ab4d5c9628d7eeb205ec635b9c18
JoonSeongPark <rytt@yonsei.ac.kr>
2020-10-22 22:14:23 +0900

    env: remove unused modules

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
@@ -57,10 +56,6 @@ module.exports = {
       template: path.join(__dirname, "..", "public", "index.html"),
     }),
     new CleanWebpackPlugin(),
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-      openAnalyzer: false,
-    }),
     new CompressionPlugin(),
   ],
 };
